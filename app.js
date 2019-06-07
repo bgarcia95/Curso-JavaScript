d6e0262a735c00f2ef443e1c582656050968b4c1
bgarcia95 <bgarcia.95@outlook.com>
2019-06-06 21:20:18 -0600

    Uso de Variables en JS

---
+++ app.js
@@ -0,0 +1,15 @@
+//var let const
+
+let nombre, mensaje;
+
+nombre = 'Juan Pablo';
+mensaje = 'Y entonces dije "Buen Curso!"';
+
+//Concatenar JavaScript
+mensaje = 'JAVA' + 'SCRIPT';
+
+//uniendo variables en una sola
+let aprendiendo = 'Aprendiendo',
+    tecnologia = 'JavaScript';
+
+console.log(`${aprendiendo} ${tecnologia}`);
