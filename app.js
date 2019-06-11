10f764ddba3e05d670def5d80a4fe076528d197c
bgarcia95 <bgarcia.95@outlook.com>
2019-06-11 13:22:03 -0600

    Numeros en JS

--- app.js
+++ app.js
@@ -1,15 +1,83 @@
 //var let const
 
-let nombre, mensaje;
+// let nombre, mensaje;
 
-nombre = 'Juan Pablo';
-mensaje = 'Y entonces dije "Buen Curso!"';
+// nombre = 'Juan Pablo';
+// mensaje = 'Y entonces dije "Buen Curso!"';
 
-//Concatenar JavaScript
-mensaje = 'JAVA' + 'SCRIPT';
+// //Concatenar JavaScript
+// mensaje = 'JAVA' + 'SCRIPT';
 
-//uniendo variables en una sola
-let aprendiendo = 'Aprendiendo',
-    tecnologia = 'JavaScript';
+// //uniendo variables en una sola
+// mensaje = 'Aprendiendo JavaScript, CSS, HTML, para ser FrontEnd';
 
-console.log(`${aprendiendo} ${tecnologia}`);
+
+
+// //console.log(mensaje.indexOf('JavaScript'));
+
+// //console.log(mensaje.split(','))
+
+// //console.log(mensaje.replace('CSS', 'PHP'));
+
+// let tecnologia = 'JavaScript'  + '\n';
+
+// console.log(tecnologia.repeat(10));
+
+//Numeros en JavaScript
+
+const numero1 = 30,
+      numero2 = 20,
+      numero3 = 20.20,
+      numero4 = .1020,
+      numero5 = -3;
+
+let resultado; 
+
+//suma
+resultado = numero1 + numero2;
+
+//resta
+resultado = numero1 - numero2;
+
+//multiplicar
+resultado = numero1 * numero2;
+
+//divisiones
+resultado = numero1 / numero2;
+
+//Modulo 
+resultado = numero1 % numero2;
+
+//Pi
+resultado = Math.PI;
+
+//redondeo
+
+resultado = Math.floor(2.99);
+
+//Raiz Cuadrada
+resultado = Math.sqrt(144);
+
+//absoluto
+resultado = Math.abs(numero5);
+
+//potencia
+
+resultado = Math.pow(8,3);
+
+//minimo
+resultado = Math.min(3,6,5,1,9,7,8);
+
+//maximo
+resultado = Math.max(3,6,5,1,9,7,8);
+
+//aleatorio
+resultado = Math.random();
+
+resultado = (10 + 20) * 5;
+
+resultado = (10 + 20 + 20 +10 + 40) * .20;
+
+let puntaje = 10;
+
+console.log(puntaje);
